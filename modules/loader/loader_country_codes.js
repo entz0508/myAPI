af0a430ab840ae91ec8b782bb22ef9a83c71d02f
entz0508 <42953980+entz0508@users.noreply.github.com>
2018-09-05 02:55:41 +0900

    commit

--- modules/loader/loader_country_codes.js
+++
@@ -1,4 +0,0 @@
-// checked by J
-const fs = require('fs');
-
-global.COUNTRY_CODES = JSON.parse(fs.readFileSync("./data/country_codes.json", "utf8")).country_codes;
--- modules/loader/loaders.js
+++
@@ -1,5 +0,0 @@
-require("./global_config.js");
-require("./loader_log.js");
-require("./loader_db_conn.js");
-require("./loader_country_codes.js");
-require("./loader_xml.js");
